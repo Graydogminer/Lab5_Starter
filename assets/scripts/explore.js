4e8f4c287d2d733436be85db283085cfa7f3eda6
Marc <mreta0002@gmail.com>
2022-10-30 23:37:19 -0700

    explore js updated

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -2,35 +2,9 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
-function init() {
-  // TODO
+const speechSynthesis = window.speechSynthesis;
 
-  textArea = document.querySelector("textarea");
-
-  populateVoiceList();
-
-  document.querySelector("button").onclick = function() {
-
-    const speakThis = new SpeechSynthesisUtterance(textArea.value);
-    speechSynthesis.speak(speakThis);
-
-  };
-
-  const image = document.querySelector("img");
-
-  if (speechSynthesis.speaking == true) {
-
-    image.src = "assets/images/smiling-open.png";
-
-  } 
-
-  if (speechSynthesis.speaking == false) {
-
-    image.src = "assets/images/smiling.png";
-
-  }
-
-}
+const voices = speechSynthesis.getVoices();
 
 function populateVoiceList() {
   
@@ -40,18 +14,68 @@ function populateVoiceList() {
 
   }
 
-  const voices = speechSynthesis.getVoices();
-
   for (let i = 0; i < voices.length; i++) {
+
     const option = document.createElement('option');
+
     option.textContent = `${voices[i].name} (${voices[i].lang})`;
 
     if (voices[i].default) {
+
       option.textContent += ' — DEFAULT';
+
     }
 
     option.setAttribute('data-lang', voices[i].lang);
+
     option.setAttribute('data-name', voices[i].name);
+
     document.getElementById("voice-select").appendChild(option);
+
   }
+
+}
+
+function init() {
+  // TODO
+
+  textArea = document.querySelector("textarea");
+
+  populateVoiceList();
+
+  const image = document.querySelector("img");
+
+  const selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+
+  document.querySelector("button").onclick = function() {
+
+    const speakThis = new SpeechSynthesisUtterance(textArea.value);
+
+    for (let i = 0; i < voices.length ; i++) {
+
+      if (voices[i].name === selectedOption) {
+  
+        speakThis.voice = voices[i];
+        
+      }
+    }
+
+    speechSynthesis.speak(speakThis);
+
+    image.src = "assets/images/smiling-open.png";
+
+  };
+
+  if (speechSynthesis.speaking == false) {
+
+    image.src = "assets/images/smiling.png";
+
+  }
+
+  if (speechSynthesis.speaking == true) {
+
+    image.src = "assets/images/smiling-open.png";
+
+  } 
+
 }
